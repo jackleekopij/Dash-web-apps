bdafaab7bede88b6d3a543a10238d4062eee1137
jackleekopij <jack.a.leekopij@gmail.com>
2018-03-11 21:01:11 +0800

    Add break and pomodoro button logic for time reset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from dash.dependencies import Event
 import dash_core_components as dcc
 import dash_html_components as html
@@ -18,10 +18,16 @@ app.config['suppress_callback_exceptions']=True
 tab_names = ["Pomodoro", "JIRA", "Long-term goals"]
 pomodoro_tabs = ["Pomodoro", "Break"]
 
+
+
+
 global minutes
 global seconds
-minutes = 0
-seconds = 10
+minutes = 25
+seconds = 0
+
+
+
 
 # Set the HTML layout for the page
 app.layout = html.Div([
@@ -37,53 +43,89 @@ app.layout = html.Div([
                 'textAlign': 'left'
             }
         ),
-        style={'width': '20%', 'float': 'left'}
+        style={'width': '20%', 'float': 'left', 'margin-top':'-150'}
     ),
-    html.Div(dcc.Tabs(
-        tabs=[{'label':'Pomodoro', 'value':"pom"},{'label':'Break', 'value':'break'} ],
-        value=0,
-        id='tabs-pomodoro',
-        vertical=False)),
+    html.Div([html.Button("Pomodoro", id="pomodoro-button", style={"height": "50", "width": "150"}),
+        html.Button("Break", id="break-button", style={"height": "50", "width": "150"})],style={'margin-top': '150'}),
     html.Div(
         html.Div(id='tab-output'),
         style={'width': '80%', 'float': 'right'}
     ),
-    dcc.Interval(id="interval-component", interval=1 * 1000),
-    html.Div(id="reset")
-],
+    html.Div(dcc.Input(id="pomodoro-task", value="Enter Pomodoro goal", type="text")),
+    html.Button("Start", id="start-timer", style={"height":"75", "width":"200"}),\
+    html.Button("Stop", id="stop-timer", style={"height":"75", "width":"200"}),
+    html.Div(id="task-name"),
+    html.Div(id="interval-start"),
+    html.Div(id="reset"),
+    html.Div(id="hidden-div"),
+    html.Div(id="break-hidden"),
+    html.Div(id="pomodoro-hidden")],
     style={
     'fontFamily': 'Sans-Serif',
     'margin-left': 'auto',
     'margin-right': 'auto',
-    'textAlign':'center',
-})
+    'textAlign':'center'}
+)
 
 
 
-@app.callback(Output('tab-output', 'children'), inputs=[Input('tabs', 'value')], events=[Event("interval-component","interval")])
-def display_content(value):
+
+
+####
+# App callbacks
+####
+
+
+
+
+@app.callback(Output("break-hidden", "children"), [Input("break-button", "n_clicks")])
+def change_to_break(click):
+    if click != None:
+        global minutes
+        global seconds
+        minutes = 0
+        seconds = 10
+
+@app.callback(Output("pomodoro-hidden", "children"), [Input("pomodoro-button", "n_clicks")])
+def change_to_break(click):
+    if click != None:
+        global minutes
+        global seconds
+        minutes = 0
+        seconds = 30
+
+
+
+
+@app.callback(Output('tab-output', 'children'), [Input('tabs', 'value')], [State("pomodoro-button", "n_clicks"), State("break-button", "n_clicks")], events=[Event("interval-component","interval")])
+def display_content(value, pomodoro_button, break_button):
     global minutes
     global seconds
-    if minutes == 25:
+    if minutes == 25 and pomodoro_button == break_button:
         interval = "25"
         minutes -= 1
-    if seconds > 0:
+        seconds = 59
+    elif seconds > 0:
         interval = str(minutes) + ":" + str(seconds)
         seconds -= 1
     else:
         seconds = 59
         minutes -= 1
+        interval = str(minutes) + ":" + str(seconds)
 
     if value == 0 and minutes >= 0:
-    #if  minutes >= 0:
-        return html.Div( [html.H1(interval),html.Div(dcc.Input(value="Enter Pomodoro goal", type="text")), html.Div(html.Button("Start", id="start-timer", style={"height":"75", "width":"200"}))],style={'margin-top': '250'})
-    elif minutes < 0:
-        return html.Div([html.H1("Pomodoro over!"),
-                         html.Div([html.Button("Reset", id="reset-button", style={"height": "75", "width": "200"}),
-                                   html.Button("Start", id="start-timer", style={"height": "75", "width": "200"})])],
+        return html.Div( [html.H1(interval)],style={'margin-top': '250'})
+    elif minutes < 0 and pomodoro_button == break_button:
+        return html.Div([html.H1("Pomodoro over!")],
+                        style={'margin-top': '250'})
+    elif minutes < 0 and pomodoro_button != break_button:
+        return html.Div([html.H1("BREAK OVER!")],
                         style={'margin-top': '250'})
     else:
         return html.Div(["NO response yet"])
+
+
+
 
 @app.callback(Output("reset", "children"), [Input("reset-button", "n_clicks")])
 def global_resets(n_clicks):
@@ -92,5 +134,50 @@ def global_resets(n_clicks):
     minutes = 0
     seconds = 60
 
+
+
+
+@app.callback(Output("hidden-div", "children"), [Input("pomodoro-button", "n_clicks"),Input("start-timer", "n_clicks")])
+def reset_counter(n_clicks, start_clicks):
+    print("Number of clicks {0}".format(n_clicks))
+    if start_clicks == None:
+        global minutes
+        global seconds
+        minutes = 0
+        seconds = 10
+
+
+
+
+@app.callback(Output("task-name", "children"), [Input("start-timer", "n_clicks")], [State("pomodoro-task", "value")])
+def get_pomodoro_goal(button, pomodoro_goal):
+    if pomodoro_goal != "Enter Pomodoro goal":
+        return html.H1(pomodoro_goal)
+    else:
+        return ""
+
+
+
+@app.callback(Output("interval-start", "children"), [Input("start-timer", "n_clicks"), Input("stop-timer", "n_clicks")])
+def start_interval(n_clicks, stop_clicks):
+    if n_clicks > 0 and n_clicks > stop_clicks :
+        return dcc.Interval(id="interval-component", interval=1 * 1000)
+    if n_clicks == stop_clicks:
+        return "Time stopped"
+    else:
+        return "Not yet started"
+
+
+
+@app.callback(Output("stop-timer","n_clicks"), [Input("pomodoro-button", "n_clicks")], [State("stop-timer", "n_clicks")])
+def stop_timer(clicks, stop_timer_clicks):
+    if clicks != None:
+        try:
+            return stop_timer_clicks + 1
+        except:
+            return 1
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
+
